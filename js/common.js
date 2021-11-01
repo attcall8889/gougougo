a99f80692a42fd8fe781c7d1ad7463265e973385
attcall8889 <93511521+attcall8889@users.noreply.github.com>
2021-11-01 16:03:06 +0800

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
-﻿/***
+/***
 *
 * 自定义解析接口
 * 视频能否正常播放的关键在于使用稳定有效的解析接口
@@ -19,7 +19,7 @@ $(function(){
 	// 搜索
 	$('#searchDo').tap(function(){
 		if(!$('#search').val()){ $('#search').myBubble('h5','请输入想看的影视名称',1000);return false; }
-		if(location.href.indexOf('http://utao520.22web.org/list/') < 0){	// 判断当前是否在list目录
+		if(location.href.indexOf('http://402i6913d8.wicp.vip/list/') < 0){	// 判断当前是否在list目录
 			location.href = '../list/search.html?keyword=' + encodeURIComponent($('#search').val());
 		}else{
 			if(location.href.indexOf('search') < 0){	// 判断当前是否未搜索页
@@ -57,10 +57,10 @@ $(function(){
 function parseTemplate(template,data){
 
 	if(data.id){
-		if(location.href.indexOf('http://utao520.22web.org/list/') < 0){	// 判断当前是否在list目录
+		if(location.href.indexOf('http://402i6913d8.wicp.vip/list/') < 0){	// 判断当前是否在list目录
 			data.href = '../item.html?vid=' + encodeURIComponent(data.id);
 		}else{
-			data.href = 'http://utao520.22web.org/item.html?vid=' + encodeURIComponent(data.id);
+			data.href = 'http://402i6913d8.wicp.vip/item.html?vid=' + encodeURIComponent(data.id);
 		}
 	}
 	if(data.from){
@@ -75,4 +75,4 @@ function parseTemplate(template,data){
 
 // 初始化js..
 var jsUrl,pageLoaded;
-$(function(){if(location.href.indexOf('http://utao520.22web.org/list/') < 0){jsUrl="http://utao520.22web.org/data.php?random=index&callback=?";}else{jsUrl="../data.php?random=index&callback=?";};if($("#jsApiList i").length){jsApi=[];$("#jsApiList i").each(function(){jsApi.push($(this).html())});if(typeof(pageLoaded)=="undefined"){pageLoad()}}else{if(jsApiList.length){jsApi=jsApiList;if(typeof(pageLoad)=="function"){pageLoad()}}else{if($.cookie("session_token")&&$.cookie("session_token")!=0){jsApi=Base64.dcode($.cookie("session_token")).split(" ");if(typeof(pageLoaded)=="undefined"){pageLoad()}}else{$.getJS(jsUrl,{act:"k",t:$.now()},function(b){try{var a=JSON.parse(b);if(parseInt(a.rt)===0){$.cookie("session_token",a.data,86400);if(a.data!=0){jsApi=Base64.dcode(a.data).split(" ")}else{jsApi=false}}else{$.cookie("session_token",0,86400);jsApi=false}}catch(c){jsApi=false}if(typeof(pageLoaded)=="undefined"){pageLoad()}})}}};})
+$(function(){if(location.href.indexOf('http://402i6913d8.wicp.vip/list/') < 0){jsUrl="http://402i6913d8.wicp.vip/data.php?random=index&callback=?";}else{jsUrl="../data.php?random=index&callback=?";};if($("#jsApiList i").length){jsApi=[];$("#jsApiList i").each(function(){jsApi.push($(this).html())});if(typeof(pageLoaded)=="undefined"){pageLoad()}}else{if(jsApiList.length){jsApi=jsApiList;if(typeof(pageLoad)=="function"){pageLoad()}}else{if($.cookie("session_token")&&$.cookie("session_token")!=0){jsApi=Base64.dcode($.cookie("session_token")).split(" ");if(typeof(pageLoaded)=="undefined"){pageLoad()}}else{$.getJS(jsUrl,{act:"k",t:$.now()},function(b){try{var a=JSON.parse(b);if(parseInt(a.rt)===0){$.cookie("session_token",a.data,86400);if(a.data!=0){jsApi=Base64.dcode(a.data).split(" ")}else{jsApi=false}}else{$.cookie("session_token",0,86400);jsApi=false}}catch(c){jsApi=false}if(typeof(pageLoaded)=="undefined"){pageLoad()}})}}};})
